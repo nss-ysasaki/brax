2978d5259450eaf20abd392244ace7e66f137745
ysasaki <73325197+nss-ysasaki@users.noreply.github.com>
2022-05-23 15:27:57 +0900

    Add inertia to the wall

--- brax/envs/fetch.py
+++ brax/envs/fetch.py
@@ -597,11 +597,10 @@ bodies {
 }
 bodies {
   name: "Obstacles"
-  colliders { position { x:0.2 y:3 z:0 } sphere { radius: 0.1 }}
-  colliders { position { x:-0.1 y:6 z:0 } sphere { radius: 0.25 }}
-  colliders { position { x:1 y:11 z:0 } sphere { radius: 1 }}
+  colliders { position { x:0.2 y:1 z:1.3 } box { halfsize { x: 1.0 y: 0.01 z: 1.0 }} color: "blue"}
   frozen { all: true }
-  mass: 1000.0
+  inertia { x: 1000. y: 1000. z: 1000. }
+  mass: 20.0
 }
 joints {
   name: "Torso_Shoulders"
