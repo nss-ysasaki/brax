37435c4e38b30e957d588d348045791f5c3a3d95
ysasaki <73325197+nss-ysasaki@users.noreply.github.com>
2022-05-23 15:27:57 +0900

    Place capsules instead of boxes (since Brax currently does not support box<>box collision)

--- brax/envs/fetch.py
+++ brax/envs/fetch.py
@@ -268,9 +268,17 @@ _SYSTEM_CONFIG = """
   }
   bodies {
     name: "Obstacles"
-    colliders { position { x:0. y:4 z:1.3 } box { halfsize { x: 2.0 y: 0.1 z: 0.5 }} color: "blue"}
-    colliders { position { x:0.5 y:7.5 z:1.3 } box { halfsize { x: 1.0 y: 0.1 z: 3. }} color: "blue"}
-    colliders { position { x:10. y:7.5 z:1.5 } box { halfsize { x: 2.0 y: 0.1 z: 0.5 }} color: "blue"}
+    colliders {
+      position { x: 0. y: 5 z: 0 }
+      capsule { radius: 1. length: 1. }
+      color: "blue"
+    }
+    colliders {
+      position { x: 0. y: 15 z: 0 }
+      capsule { radius: 1. length: 3. }
+      rotation { x: 90 }
+      color: "blue"
+    }
     frozen { all: true }
     inertia { x: 1000. y: 1000. z: 1000. }
     mass: 20.0
